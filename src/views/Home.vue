a9de089c9b6a2aafc9f0a4da3d87e9aa16ed39bd
Lopez Briceno, Frank <frank.lopez.briceno@accenture.com>
2021-07-04 21:35:45 -0600

    Editing Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+   <h2 class="text-center">¡Bievenido a la wiki usando Vue.js!</h2>
+   <div class="d-flex justify-content-center mt-5">
+     <div>
+       <router-link class="btn btn-success" to="/categorias">Ver Categorías</router-link>
+     </div>
+   </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
